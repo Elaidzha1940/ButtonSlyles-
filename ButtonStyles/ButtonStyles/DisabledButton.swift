302646f9b39d46dc7f40b8c0be20334f1d95201f
Elaidzha1940 <eli080293@gmail.com>
2023-03-26 19:17:46 +0700

    added a Ne File + buttonDisabled

---
+++ ButtonStyles/ButtonStyles/DisabledButton.swift
@@ -0,0 +1,27 @@
+//
+//  DisabledButton.swift
+//  ButtonStyles
+//
+//  Created by Elaidzha Shchukin on 26.03.2023.
+//
+
+import SwiftUI
+
+struct DisabledButton: View {
+    
+    @State private var buttonDisabled: Bool = false
+    
+    var body: some View {
+        
+        VStack {
+            
+            
+        }
+    }
+}
+
+struct DisabledButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DisabledButton()
+    }
+}

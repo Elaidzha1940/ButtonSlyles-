ded617e6963be893db9293e063eca785ea827140
Elaidzha1940 <eli080293@gmail.com>
2023-03-26 18:48:34 +0700

    Button Styles

---
+++ ButtonStyles/ButtonStyles/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  ButtonStyles
+//
+//  Created by Elaidzha Shchukin on 26.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+            Button("+ Button +") {
+                
+                
+            }
+            
+        }
+        //.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
